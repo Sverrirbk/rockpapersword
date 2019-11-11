8c88024cab89d43f622d56f3c595d44c074391e4
Sverrirbk <sverrirbk@gmail.com>
2019-11-11 01:00:01 +0100

    Game is 100% functional.

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ var cMove = document.querySelector("#cMove");
 // Computers random move
 function computerPlay() {
     let result = Math.floor(Math.random() * 3);
-    // debugging console.log(result); 
     if (result == 0){
         move = "rock";
     }
@@ -22,7 +21,6 @@ function computerPlay() {
     else {
         move = "sword";
     }
-    // debugging console.log(move);
     return move;
 }
 
@@ -30,48 +28,55 @@ function computerPlay() {
 // Button press
 function buttonPress(e){
     if (e.id == "rock" || e.id == "paper" || e.id == "sword") {
-        console.log("You have clicked button id = " + e.id + "");
         playerChoice = e.id;
-        console.log(playerChoice);
         game(playerChoice);
         
         
     }
     // Reset button
     else {
-        let playerScore = 0;
-        let computerScore = 0;
-        let drawScore = 0;
-        draws.innerHTML = drawScore;
-        pWins.innerHTML = playerScore;
-        cWins.innerHTML = computerScore;
-    }
+        playerScore = 0;
+        computerScore = 0;
+        drawScore = 0;
+        pWins.innerHTML = "Player wins";
+        cWins.innerHTML = "Computer wins";
+        draws.innerHTML = "Draws";
+        document.getElementById("rock").disabled = false;
+        document.getElementById("paper").disabled = false;
+        document.getElementById("sword").disabled = false;
+
+
+        var imgs = document.getElementsByTagName("img");
+        for(var i=0, l=imgs.length; i<l; i++) {
+            imgs[i].src = "question.png";
+        }
+}
+    
 }
 
 // Play a round
 function game(playerChoice) { 
      
-        compMove = computerPlay();
-        playerMove = playerChoice;
-        let round = (RockPaperSword(playerMove, compMove));
+    compMove = computerPlay();
+    playerMove = playerChoice;
+    let round = (RockPaperSword(playerMove, compMove));
 
-        updateMoves(compMove, playerMove)
-        if (round == "draw") {
-            console.log("It was a draw");
-        }
+    updateMoves(compMove, playerMove)
+    if (round == "draw") {
+    }
 
-        else if (round == "win") {
-            console.log("Player wins!")
-        }
+    else if (round == "win") {
+    }
 
-        else if (round == "loss") {
-            console.log("Computer wins")
-        }
-        
-        else {
-            console.log("ERROR")
-        }
-        updateScore(round); 
+    else if (round == "loss") {
+    }
+    
+    else {
+        console.log("ERROR")
+    }
+    updateScore(round);
+    // Check if game is over
+    checkWinner(playerScore, computerScore);
 }
 
 // Get results
@@ -81,64 +86,74 @@ function RockPaperSword(player, computer) {
     let pWins;
     let cWins;
     let draws;
+    updateChoice(player, computer)
     if (player == "rock") {
         if (computer =="rock") {
-            console.log("Draw!");
             return "draw";
         }
         else if (computer == "paper") {
-            console.log("You lose! Paper beats rock");
             return "loss";
         }
         else {
-            console.log("You win! Rock beats sword");
             return "win";
         }
     }
 
     else if (player == "paper") {
         if (computer == "rock") {
-            console.log("You win! Paper beats rock");
             return "win"
         }
         else if (computer == "paper") {
-            console.log("Draw!");
             return "draw";
         }
         else {
-            console.log("You lose! sword beat paper");
             return "loss";
         }
     }
 
     else if (player == "sword") {
         if (computer == "rock") {
-            console.log("You lose! Rock beats sword");
             return "loss";
         }
         else if (computer == "paper") {
-            console.log("You win! sword beat paper");
             return "win";
         }
         else {
-            console.log("Draw!");
             return "draw";
         }
     }
 } 
+function updateChoice(player, computer) {
+        
+    
+    playerPick.innerHTML = "Player picked " + player.toUpperCase();
+    document.getElementById("pPick").src = player + ".png"; 
+    var playerImg = document.createElement('img')
+    playerImg.src = player + ".png";
+    document.getElementById('playerPickDiv').appendChild(playerImg);
+
+    computerPick.innerHTML = "Computer picked " + computer.toUpperCase();
+    document.getElementById("cPick").src = computer + ".png";
+    var compImg = document.createElement('img')
+    compImg.src = computer + ".png";
+    document.getElementById('computerPickDiv').appendChild(compImg);
+}
 
 function updateScore(results) {
     if (results == "win"){
         playerScore++;
         pWins.innerHTML = "Player wins " + playerScore;
+        roundResult.innerHTML = "Player wins this round!";
     }
     else if (results == "loss") {
         computerScore++;
         cWins.innerHTML = "Computer wins " + computerScore;
+        roundResult.innerHTML = "Computer wins this round!";
     }
     else {
         drawScore++;
         draws.innerHTML = "Draws " + drawScore;
+        roundResult.innerHTML = "This round is a draw!";
 
     }
 }
@@ -146,4 +161,23 @@ function updateScore(results) {
 function updateMoves(computer, player) {
     document.getElementById("cMove").src = computer + ".png";
     document.getElementById("pMove").src = player + ".png"; 
+}
+
+function checkWinner(playerScore, computerScore) {
+    if (playerScore >= 5) {
+        disableButtons();
+        roundResult.innerHTML = "Player won the game!";
+        document.getElementById("pMove").src = "playerWin.png"; 
+}
+    else if (computerScore >= 5) {
+        disableButtons();
+        roundResult.innerHTML = "You lost the game!";
+        document.getElementById("pMove").src = "computerWin.png";
+    }
+}
+
+function disableButtons (computerWin, playerWin) {
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("sword").disabled = true;
 }
